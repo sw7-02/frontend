8a713b96095817e1025f1009b59c4b769f25036e
Lasse Andersen <lran18@student.aau.dk>
2023-11-07 09:25:39 +0100

    buttons in navigation bar appear according to route

--- src/lib/components/NavigationBar.svelte
+++ src/lib/components/NavigationBar.svelte
@@ -1,8 +1,36 @@
-<script>
+<script lang="ts">
+	import { page } from '$app/stores';
 	import NavigationButton from './NavigationButton.svelte';
+
+	let paths: string[] = [];
+	let routes: string[] = [];
+	let navigation: { path: string; route: string }[] = [];
+
+	$: {
+		paths = $page.url.pathname.split('/');
+		routes = [];
+		navigation = [];
+
+		paths = paths.filter((path) => path !== '' && path !== 'course');
+
+		for (let i = 0; i < paths.length; i++) {
+			let path = paths[i];
+			let route = '/course/' + path;
+
+			routes.push(route);
+			navigation.push({ path, route });
+		}
+		console.log(navigation);
+	}
 </script>
 
 <nav class="flex justify-between bg-neutral-800 h-14 shadow-xl">
-	<NavigationButton text={'Courses'} href={'/'} />
+	<div class="flex">
+		<NavigationButton text={'Courses'} href={'/'} />
+		{#each navigation as nav}
+			<p class="text-neutral-300 text-xl pt-4 pb-4">/</p>
+			<NavigationButton text={nav.path} href={nav.route} />
+		{/each}
+	</div>
 	<NavigationButton text={'Log out'} href={'/test'} />
 </nav>
