8255fa368148633f433203fe58878ef13803bdba
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2023-11-08 10:28:31 +0100

    Course overview

--- src/lib/components/Square.svelte
+++ src/lib/components/Square.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
-	export let title: string = 'title';
-	export let href: string = '/';
+    import { createEventDispatcher } from 'svelte';
+	import Modal from './Modal.svelte';
+
+	let showModal: boolean = false;
+    export let title: string = 'title';
+    export let href: string = '/';
+	export let deleted: boolean = false;
+
+	function onSubmit() {
+		
+	}
 </script>
 
-<a
-	{href}
-	class="bg-neutral-700 border-neutral-900 border-2 m-6 grid grid-cols-1 items-center
-			shadow-xl rounded-md text-neutral-300 text-3xl w-60 h-60"
->
-	<div class="flex justify-center">{title}</div>
+
+<a {href} class="bg-neutral-700 border-neutral-900 border-2 m-6 grid
+			shadow-xl rounded-md text-neutral-300 w-60 h-60">
+	<div class="grid grid-flow-col justify-end">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div class="flex float-right">
+			<i class="fa-regular fa-trash-can fa-ss m-3" on:click|preventDefault={() => deleted = true} />
+			<i class="fa-solid fa-pencil fa-ss m-3" on:click|preventDefault={() => showModal = true} />
+		</div>
+	</div>
+	
+	<p class="text-3xl flex justify-center"> {title} </p>
 </a>
+
+<Modal bind:showModal submitCallback={onSubmit}>
+	<div class="flex flex-col grid-cols-1 justify-items-center">
+		<input
+			class="bg-neutral-500 m-2 w-[316] text-neutral-300"
+			type="text"
+			placeholder="Title"
+			bind:value={title}
+		/>
+	</div>
+</Modal>
