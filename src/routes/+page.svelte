0362d9b7766baabdf0687281bd6f602f340aef07
Lasse Andersen <lran18@student.aau.dk>
2023-11-10 12:15:51 +0100

    made the function for adding a new course into a modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
-<script>
+<script lang="ts">
 	import Square from '$lib/components/Square.svelte';
+	import Modal from '$lib/components/Modal.svelte';
 
 	let squares = [{ title: 'IMPR' }, { title: 'OOP' }, { title: 'PP' }];
+
+	let showModal: boolean = false;
+	let newSquareTitle: string = '';
 
 	// TODO: Need to somehow update list of courses when deleting it
 	// (possibly use the "deleted" variable in the component Square)
 
-	function addSquare() {
+	function onSubmit() {
 		// TODO: Put the new course in the database and fetch all courses
-		squares = [...squares, { title: 'new' }];
+		squares = [...squares, { title: newSquareTitle }];
+		newSquareTitle = '';
 	}
 </script>
 
@@ -21,12 +26,23 @@
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-static-element-interactions -->
 		<div
-			on:click={addSquare}
+			on:click={() => (showModal = true)}
 			class="bg-neutral-600 border-neutral-700 border-2 m-6 justify-center items-center flex
 			shadow-xl rounded-md text-neutral-300 w-60 h-60"
 			style="cursor: pointer;"
 		>
-			<i class="fa-regular fa-plus text-9xl text-neutral-700" />
+			<i class="fa-regular fa-plus text-7xl text-neutral-700" />
 		</div>
 	</div>
 </div>
+
+<Modal bind:showModal submitCallback={onSubmit}>
+	<div class="flex flex-col grid-cols-1 justify-items-center">
+		<input
+			class="bg-neutral-500 m-2 w-[316] text-neutral-300"
+			type="text"
+			placeholder="Title"
+			bind:value={newSquareTitle}
+		/>
+	</div>
+</Modal>
