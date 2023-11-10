fd70b06559354007062e52a5274fe5c352178cd0
Lasse Andersen <lran18@student.aau.dk>
2023-11-10 14:17:34 +0100

    removed codemirror package for code editor

--- src/routes/course/[course]/[task]/[exercise]/+page.svelte
+++ src/routes/course/[course]/[task]/[exercise]/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import CodeMirror from 'svelte-codemirror-editor';
-	import { javascript } from '@codemirror/lang-javascript';
-	import { stringify } from 'postcss';
 
 	// Assignements and exercises
 	const name: string = 'Exercise 1: Hello World';
@@ -24,6 +21,6 @@
 		<p class="text-neutral-300 text-2xl m-2 underline">{name}</p>
 	</div>
 	<div class="bg-neutral-700 border-neutral-900 border-2 mt-4 mb-4 w-[900px]">
-		<CodeMirror bind:value lang={javascript()} tabSize={4} />
+		<!-- insert editor -->
 	</div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	optimizeDeps: {
-		exclude: ["codemirror", "@codemirror/lang-javascript"],
-	}
 };
 
 export default config;
