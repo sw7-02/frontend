7a9fff2cb085bb0a2f68066493f0cb3d38391463
Lasse Andersen <lran18@student.aau.dk>
2023-11-11 15:09:15 +0100

    changed background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import NavigationBar from '$lib/components/NavigationBar.svelte';
 </script>
 
-<body class="bg-neutral-950 overflow-hidden bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-zinc-700  to-black">
+<body class="bg-neutral-950 overflow-hidden bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-zinc-700  to-neutral-950">
 	<NavigationBar />
 	<div class="overflow-auto" style="height: calc(100vh - 56px);">
 		<slot />
