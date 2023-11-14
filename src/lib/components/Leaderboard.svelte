fdff7406feb9a66c922e652094bd97e84d0227ae
Lasse Andersen <lran18@student.aau.dk>
2023-11-14 10:49:18 +0100

    added components for leaderboard and statistics

---
+++ src/lib/components/Leaderboard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    let table_data: { name: string; points: number }[] = [
+        { name: "John", points: 100 },
+        { name: "Jane", points: 200 },
+        { name: "Bob", points: 50 },
+    ];
+</script>
+
+<div class="bg-neutral-900 mt-4 mb-4 mr-4 w-[900px]">
+    <table class="w-full m-4">
+        <thead>
+            <tr class="text-center font-mono text-neutral-100 text-md underline">
+                <th>Name</th>
+                <th>Points</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each table_data as { name, points }}
+                <tr class="text-center font-mono text-green-700 text-md">
+                    <td>{name}</td>
+                    <td>{points}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
--- src/routes/course/[course]/Leaderboard/+page.svelte
+++ src/routes/course/[course]/Leaderboard/+page.svelte
@@ -1,1 +1,14 @@
-<p class="text-2xl">leaderboard</p>
+<script>
+    import Leaderboard from "$lib/components/Leaderboard.svelte";
+    import Statistics from "$lib/components/Statistics.svelte";
+</script>
+
+<div
+    class="grid grid-cols-1 justify-items-center overflow-hidden"
+    style="height: calc(100vh - 64px);"
+>
+    <div class="flex overflow-hidden">
+        <Leaderboard />
+        <Statistics />
+    </div>
+</div>
