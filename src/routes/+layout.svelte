dd60d2d28a561fddd2086da320d2c41a0d2660fb
Lasse Andersen <lran18@student.aau.dk>
2023-11-20 17:04:19 +0100

    hacker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,18 @@
     import NavigationBar from "$lib/components/NavigationBar.svelte";
 </script>
 
-<body
-    class="bg-neutral-950 overflow-hidden bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-zinc-700 to-neutral-950"
->
+<!-- <body class="overflow-hidden bg-neutral-950 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-zinc-700 to-neutral-950"> -->
+<body class="overflow-hidden bg-neutral-950 bg-opacity-90">
+    <video
+        class="absolute w-full h-full object-cover -z-10"
+        muted
+        autoplay
+        loop
+        playsinline
+        disablepictureinpicture
+    >
+        <source src="/hacker.mp4" type="video/mp4" />
+    </video>
     {#if $authentication.isAuthenticated}
         <NavigationBar />
         <div class="overflow-auto" style="height: calc(100vh - 64px);">
