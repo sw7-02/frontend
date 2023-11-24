944ac3c272a5865b8fea48a0c11d8502594ff5ea
Lasse Andersen <lran18@student.aau.dk>
2023-11-24 10:13:55 +0100

    authentication store

--- src/lib/stores/authentication.ts
+++ src/lib/stores/authentication.ts
@@ -1,19 +1,9 @@
 import { writable } from "svelte/store";
 
-// TODO: Replace/implement JWT authentication
-
-type User = {
-    username: string;
-    password: string;
-    role: 0 | 1 | 2;
-};
-
 type Authentication = {
-    isAuthenticated: boolean;
-    user: User;
+    jwtToken: string;
 };
 
 export const authentication = writable<Authentication>({
-    isAuthenticated: false,
-    user: { username: "", password: "", role: 2 },
+    jwtToken: "",
 });
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,14 +1,14 @@
-// export async function load() {
-//     return await fetch("http://localhost:8080/prismatest", {
-//         method: "GET",
-//     }).then((response) => {
-//         if (response.ok) {
-//             return response.json().then((json) => {
-//                 console.log(json);
-//                 return json;
-//             }).catch((error) => { console.log(error); return { error: "Failed to parse data" }; });
-//         } else {
-//             return { error: "Failed to fetch data" };
-//         }
-//     });
-// }
+export async function load() {
+    await fetch("http://localhost:8080/login", {
+        method: "POST",
+    }).then((response) => {
+        if (response.ok) {
+            return response.json().then((json) => {
+                console.log(json);
+                return json;
+            }).catch((error) => { console.log(error); return { error: "Failed to parse data" }; });
+        } else {
+            return { error: "Failed to fetch data" };
+        }
+    });
+}
