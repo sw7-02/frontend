411c927108db45668a4d354ca2fe41bc29d94b8f
Lasse Andersen <lran18@student.aau.dk>
2023-12-02 12:04:35 +0100

    update to backgrounds

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -8,7 +8,7 @@
         top: 0;
         left: 0;
         z-index: -1;
-        background: linear-gradient(45deg, #000509, #01142b, #061b2c);
+        background: linear-gradient(45deg, #0b2133, #0e1825, #0f3553);
         overflow: hidden;
     }
 </style>
