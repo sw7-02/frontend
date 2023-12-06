1fad6b3427f76d8a68c3e0884bd67d5c7ee49095
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2023-12-06 12:24:47 +0100

    CD (#19)
    * fmt
    
    * CD
    
    * fmt

--- src/lib/stores/ids.ts
+++ src/lib/stores/ids.ts
@@ -1,5 +1,11 @@
 import { persistentWritable } from "./localStorage";
 
-export const courseIdStore = persistentWritable<number | null>("courseId", null);
-export const sessionIdStore = persistentWritable<number | null>("sessionId", null);
+export const courseIdStore = persistentWritable<number | null>(
+    "courseId",
+    null
+);
+export const sessionIdStore = persistentWritable<number | null>(
+    "sessionId",
+    null
+);
 export const taskIdStore = persistentWritable<number | null>("taskId", null);
